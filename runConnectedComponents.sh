554b5d8b8a20a65b61d691ce8db546104be48fda
liscju <piotr.listkiewicz@gmail.com>
2016-01-13 01:31:35 +0100

    Add simple bash runners

---
+++ runConnectedComponents.sh
@@ -0,0 +1,4 @@
+/home/liscju/Programy/spark-1.6.0/bin/spark-submit --class SparkConnectedComponents \
+    --master local[*] \
+    /home/liscju/AlgorytmyRownolegle/Lab3/target/scala-2.10/connectedcomponents_2.10-1.0.jar \
+    $1
