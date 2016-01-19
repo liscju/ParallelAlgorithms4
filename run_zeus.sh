6f18727349b4870930fc279e1f9843fa8f5afef5
liscju <piotr.listkiewicz@gmail.com>
2016-01-19 23:59:47 +0100

    Add starting multinode spark cluster in zeus run configuration

--- run_zeus.sh
+++ run_zeus.sh
@@ -2,8 +2,14 @@
 #PBS -l nodes=1:ppn=12
 module load plgrid/apps/spark
 
+source $PLG_GROUPS_STORAGE/plgg-spark/set_env_spark-1.0.0.sh
+
+$SPARK_HOME/sbin/start-multinode-spark-cluster.sh
+
 $SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] /people/plgliscju/ParallelAlgorithms4/target/scala-2.10/connectedcomponents_2.10-1.0.jar /people/plgliscju/ParallelAlgorithms4/graphs/g10.txt > /people/plgliscju/ParallelAlgorithms4/output/g10.out
 $SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] /people/plgliscju/ParallelAlgorithms4/target/scala-2.10/connectedcomponents_2.10-1.0.jar /people/plgliscju/ParallelAlgorithms4/graphs/g100.txt > /people/plgliscju/ParallelAlgorithms4/output/g100.out
 $SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] /people/plgliscju/ParallelAlgorithms4/target/scala-2.10/connectedcomponents_2.10-1.0.jar /people/plgliscju/ParallelAlgorithms4/graphs/g1000.txt > /people/plgliscju/ParallelAlgorithms4/output/g1000.out
 $SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] /people/plgliscju/ParallelAlgorithms4/target/scala-2.10/connectedcomponents_2.10-1.0.jar /people/plgliscju/ParallelAlgorithms4/graphs/g10000.txt > /people/plgliscju/ParallelAlgorithms4/output/g10000.out
 $SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] /people/plgliscju/ParallelAlgorithms4/target/scala-2.10/connectedcomponents_2.10-1.0.jar /people/plgliscju/ParallelAlgorithms4/graphs/g100000.txt > /people/plgliscju/ParallelAlgorithms4/output/g100000.out
+
+$SPARK_HOME/sbin/stop-multinode-spark-cluster.sh
